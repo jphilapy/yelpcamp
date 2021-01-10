e82ff6f39e34a8abd37efb731248042bfb02632a
Jeff Philapy <jeff@altahost.com>
2021-01-10 15:56:07 -0500

    adding edit/new functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 const Campground = require('./models/campground')
 
 
@@ -22,6 +23,8 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // routes
 app.get('/', (req, res) => {
@@ -34,12 +37,31 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     // console.log(req.params.id)
     res.render('campgrounds/show', { campground })
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    // console.log(req.params.id)
+    res.render('campgrounds/edit', { campground })
+})
+app.put('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 // server
 app.listen(3000, () => {
     console.log('server running')
